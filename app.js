49fcee2bbe682825453b2dc119962949dd2b5afa
Craig Barratt <craigtbarratt@gmail.com>
2020-04-17 22:20:54 +0100

    initial commit of 3D car

---
+++ app.js
@@ -0,0 +1,11 @@
+// variables for setup
+
+let container;  //on html page
+let camera;
+let renderer; //renders 3D
+let scene; //3D world scene
+let car;
+
+function init(){
+  container = document.querySelector('.scene');
+}
